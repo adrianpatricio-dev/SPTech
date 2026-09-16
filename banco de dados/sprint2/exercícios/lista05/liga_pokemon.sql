17a2d2aec6356d47202b59e26d4ef00fffd61f81
adrianpatricio-dev <adrian.silva@sptech.school>
2026-09-16 14:13:57 -0300

    feat: adiciona funcionalidades restantes do exercício "liga_pokemon"

--- banco de dados/sprint2/exercícios/lista05/liga_pokemon.sql
+++ banco de dados/sprint2/exercícios/lista05/liga_pokemon.sql
@@ -102,3 +102,111 @@ SELECT
 	nome,
     NULLIF(idade, 12) AS idade_diferente_de_12
 FROM treinador;
+
+
+
+-- ALTER TABLE COM MÚLTIPLAS ALTERAÇÕES --
+
+-- 1)
+ALTER TABLE treinador ADD COLUMN 
+	cidade VARCHAR(50), 
+    DROP COLUMN telefone;
+
+-- 2)
+DESCRIBE treinador;
+
+
+
+-- UPDATE : ATUALIZAÇÃO DE RESGITROS --
+
+-- 1)
+UPDATE treinador SET 
+	nome = 'Ash', 
+	idade = 13, 
+    cidade = 'Pallet'
+WHERE id_treinador = 100;
+
+SELECT * FROM treinador;
+
+-- 2)
+UPDATE treinador SET 
+	cidade = 'Pallet'  
+WHERE id_treinador IN (101, 103);
+
+SELECT * FROM treinador;
+
+-- 3)
+UPDATE treinador SET 
+	cidade = 'São Paulo' 
+WHERE cidade IS NULL;
+
+SELECT * FROM treinador;
+
+-- 4)
+SELECT * FROM treinador WHERE id_treinador BETWEEN 101 AND 103 OR id_treinador = 100;
+
+
+
+-- TABELA pokemon COM DEFAULT
+
+-- 1)
+CREATE TABLE pokemon (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    tipo VARCHAR(45) DEFAULT 'Água',
+    nivel TINYINT,
+    hp INT,
+    atk INT
+);
+
+-- 2)
+INSERT INTO pokemon (nome, nivel, hp, atk) VALUES
+	('Squirtle', 2, 30, 10),
+	('Greninja', 50, 120, 140),
+	('Lapras', 40, 90, 100);
+    
+-- 3)
+SELECT * FROM pokemon;
+
+-- 4)
+INSERT INTO pokemon (nome, nivel, hp, atk, tipo) VALUES
+	('Pikachu', 100, 100, 70, 'Elétrico'),
+	('Zapdos', 70, 500, 300, 'Elétrico'),
+	('Alakazam', 25, 120, 70, 'Psíquico');
+    
+    
+    
+-- OPERAÇÕES ARITMÉTICAS NO SELECT --
+
+-- 1)
+SELECT
+	nome,
+    hp + atk AS poder_total
+FROM pokemon;
+
+-- 2)
+SELECT
+	nome,
+    hp - atk AS total
+FROM pokemon;
+
+-- 3)
+SELECT
+	nome,
+    hp * 2 AS dobra_da_vida,
+    atk / 2 AS ataque_reduzido,
+    atk
+FROM pokemon;
+
+
+
+-- ALTER TABLE : REMOVER E RECRIAR COLUNAS --
+
+-- 1)
+ALTER TABLE pokemon ADD COLUMN 
+	tipo VARCHAR(20), -- COLUNA NÍVEL JÁ EXISTE
+    DROP COLUMN nivel,
+    ADD COLUMN nivel INT;
+
+-- 2)
+DESCRIBE pokemon;
